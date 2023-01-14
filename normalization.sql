5036dbccdbd384d056162be5eec46ebd61d52194
Anna Axelsson <a.axelsson51@gmail.com>
2023-01-15 00:03:39 +0100

    create UNF table

---
+++ normalization.sql
@@ -0,0 +1,20 @@
+
+
+/********** UNF **********/
+
+USE iths;
+
+DROP TABLE IF EXISTS UNF;
+
+CREATE TABLE `UNF` (
+    `Id` DECIMAL(38, 0) NOT NULL,
+    `Name` VARCHAR(26) NOT NULL,
+    `Grade` VARCHAR(11) NOT NULL,
+    `Hobbies` VARCHAR(25),
+    `City` VARCHAR(10) NOT NULL,
+    `School` VARCHAR(30) NOT NULL,
+    `HomePhone` VARCHAR(15),
+    `JobPhone` VARCHAR(15),
+    `MobilePhone1` VARCHAR(15),
+    `MobilePhone2` VARCHAR(15)
+)  ENGINE=INNODB;
