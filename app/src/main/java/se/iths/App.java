5d5befe97f9c34ce1b94735ab5bbfefb92d7a906
Anna Axelsson <a.axelsson51@gmail.com>
2023-01-14 23:59:36 +0100

    add Crud to AppTest

---
+++ app/src/main/java/se/iths/App.java
@@ -0,0 +1,26 @@
+package se.iths;
+
+import java.sql.*;
+
+public class App {
+  
+  public static void main(String[] args){
+    /*System.out.println("Hello ITHS");
+    try {
+      Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Chinook iths, iths");
+      System.out.println("Hej vi klarade att koppla upp");
+      ResultSet rs = con.createStatement().executeQuery(SELECT_ALL_ARTISTS);
+      System.out.println("Vi klarade köra frågan");
+      while(rs.next()){
+        long artistId = rs.getLong("ArtistId");
+        String name = rs.getString("Name");
+        System.out.println(new Artist(artistId, Name));
+      }
+    }
+    catch(SQLException e){
+      e.printStackTrace();
+      System.err.println("Hej det gick fel " + e.getMessage());
+    }*/
+  }
+
+}
